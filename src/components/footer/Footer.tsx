d30e60f140a805bf35282b3f43d5d15baf86c2e4
AleVuj <40275796+AleVuj@users.noreply.github.com>
2023-07-22 15:51:19 +0200

    first com

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Alexander</span>
+      <span>Alexander's admin board</span>
+    </div>
+  );
+};
+
+export default Footer;
